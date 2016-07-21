9dedbf5a1ee30867e1cfd9af47a4ac5e243c32c5
Hoang Kien <kienhg96@gmail.com>
2016-07-21 04:21:01 +0000

    fix authenticate

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,7 @@ module.exports = function(passport){
         res.render('index', {title: 'Home'});
     });
     router.post('/login', function(req, res) {
-        passport.authenticate('local', function(err, user, info) {
+        passport.authenticate('login', function(err, user, info) {
             if (err) { 
                 throw err;
             }
@@ -16,33 +16,26 @@ module.exports = function(passport){
                 return res.json({errCode: -1, msg: 'Login Failed'}); 
             }
             req.logIn(user, function(err) {
-                if (err) { 
-                    throw err; 
-                }
+                if (err) throw err; 
                 return res.json({errCode: 0, msg: 'Login success', user: {username: user.username, id: user._id.toString()}});
             });
         })(req, res);
     });
-
+    
     
     router.post('/signup', function(req, res){
-        var username = req.body.username;
-        var password = req.body.password;
-        mongo.connect(process.env.MONGO_URI, function(err, db){
-            if (err) throw err;
-            db.collection('user').findOne({username: username}, function(err, user){
+        passport.authenticate('signup', function(err, user, info){
+            if (err) {
+                throw err;
+            }
+            if (!user) {
+                return res.json({errCode: -2, msg: 'Username Already exists'})
+            }
+            req.logIn(user, function(err){
                 if (err) throw err;
-                if (user) {
-                    return res.json({errCode: -2, msg: 'Username already exists'});
-                }
-                db.collection('user').insert({username: username, password: password}, function(err, data){
-                    db.close();
-                    if (err) throw err;
-                    var user = data.ops[0];
-                    res.json({errCode: 0, msg: 'Signup Success', user: user});
-                });
+                return res.json({errCode: 0, msg: 'Signup Success'});
             });
-        });
+        })(req, res); 
     });
     
     router.get('/logout', function(req, res){
@@ -63,6 +56,7 @@ module.exports = function(passport){
             res.json(null);
         }
     })
+    
     
     return router;
 }
